893d581f536a18b7172a899405f08cd3f449430d
Sarietha <sarietha.coding@gmail.com>
2024-03-13 15:55:44 +0200

    Reworked the look and feel and cleaned up js code

--- rps.js
+++ rps.js
@@ -10,13 +10,14 @@ function resetGame(){
     computerScore = 0;
     playerScore = 0;
 
-    document.querySelector('#pc-score').textContent = "PC : " + computerScore;
-    document.querySelector('#player-score').textContent = "Player : " + playerScore;
+    document.querySelector('#pc-score').textContent = computerScore;
+    document.querySelector('#player-score').textContent = playerScore;
 
     document.querySelector('#game').textContent = "New Game Started"
 
     // To do - clear images
      // Remove old image (by changing the class) and set the new image for selected R/P/S for PC
+     /*
      let currentClass = document.querySelector("#pcimg").classList;
      if(currentClass != ''){    
          document.querySelector("#pcimg").classList.remove(currentClass); 
@@ -26,7 +27,10 @@ function resetGame(){
     let currentPClass = document.querySelector("#playerimg").classList;
     if (currentPClass != '') {
          document.querySelector("#playerimg").classList.remove(currentPClass);
-    }
+    }*/
+
+    document.querySelector("#pcimg").classList = "noimg";
+    document.querySelector("#playerimg").classList = "noimg";
     
 
     gameOver = false;
@@ -50,68 +54,73 @@ function playRound(playerSelection, computerSelection){
     let pSelection = playerSelection.toLowerCase();
     let cSelection = computerSelection.toLowerCase();
 
+    let playerWins = false;
+    let pcWins = false;
+
     // Remove old image (by changing the class) and set the new image for selected R/P/S for PC
-    let currentClass = document.querySelector("#pcimg").classList;
-    if(currentClass != ''){    
-        document.querySelector("#pcimg").classList.remove(currentClass); 
-    }
-    document.querySelector("#pcimg").classList.add(cSelection);
+    document.querySelector("#pcimg").classList = cSelection;
 
     // Remove old image (by changing the class) and set the new image for selected R/P/S for Player
-   let currentPClass = document.querySelector("#playerimg").classList;
-   if (currentPClass != '') {
-        document.querySelector("#playerimg").classList.remove(currentPClass);
-   }
-   
-   document.querySelector("#playerimg").classList.add(pSelection);
+    document.querySelector("#playerimg").classList = pSelection;
 
     //console.log(pSelection + " vs " + cSelection);
     
     // Both picked the same - it's a draw
     if (pSelection === cSelection) {
-        document.querySelector('#game').textContent = ("It's a draw! You both choose " + computerSelection);
-        computerScore = computerScore + 1;
-        playerScore = playerScore + 1;
+        playerWins = true;
+        pcWins = true;
     }
-
 
     else if (cSelection === "rock") {
         if (pSelection === "scissors") {
-            // rock beats scissors, pc wins
-            document.querySelector('#game').textContent = ("Rock beats Scissors - Computer wins!");
-            computerScore = computerScore + 1;
+            pcWins = true;            
         }
         else if (pSelection === "paper") {
-            document.querySelector('#game').textContent = ("Paper covers Rock - Player wins!");
-            playerScore = playerScore + 1;
+            playerWins = true;
         }
     }
 
     else if (cSelection === "paper")     {
         if (pSelection === "scissors") {
-            document.querySelector('#game').textContent = ("Scissors cut Paper - Player wins!");
-            playerScore = playerScore + 1;
+            playerWins = true;
         }
         else if (pSelection === "rock") {
-            document.querySelector('#game').textContent = ("Paper covers Rock - Computer wins!");
-            computerScore = computerScore + 1;
+            pcWins = true;
         }
     }
 
     else if (cSelection === "scissors") {
         if (pSelection === "paper") {
-            document.querySelector('#game').textContent = ("Scissors cut Paper - Computer wins!");
-            computerScore = computerScore + 1;
+            pcWins = true;
         }
         else if (pSelection === "rock") {
-            document.querySelector('#game').textContent = ("Rock beats Scissors - Player wins!");
-            playerScore = playerScore + 1;
+            playerWins = true;
         }
     }
 
+
+    // Calculate scores, indicate winning hand and print winning text
+    if(playerWins && pcWins) {
+        document.querySelector('#playerimg').classList.add("winner");
+        document.querySelector('#pcimg').classList.add("winner");
+        document.querySelector('#game').textContent = ("It's a draw");
+        playerScore = playerScore + 1;
+        computerScore = computerScore + 1;
+    }
+    else if(playerWins) {
+        playerScore = playerScore + 1;
+        document.querySelector('#playerimg').classList.add("winner");
+        document.querySelector('#game').textContent = ("Player wins");
+    }
+    else if(pcWins) {
+        computerScore = computerScore + 1;
+        document.querySelector('#pcimg').classList.add("winner");
+        document.querySelector('#game').textContent = ("Computer wins");
+    }
     
-    document.querySelector('#pc-score').textContent = "PC : " + computerScore;
-    document.querySelector('#player-score').textContent = "Player : " + playerScore;
+    
+    document.querySelector('#pc-score').textContent = computerScore;
+    document.querySelector('#player-score').textContent = playerScore;
 
 }
 
@@ -124,20 +133,20 @@ function playGame(playerChoice) {
         let playerSelection = playerChoice;
         playRound(playerSelection, computerSelection);
     
-        // End game after player/pc scores 10
-        if ((playerScore === 10) || (computerScore === 10)) {
+        // End game after player/pc scores 5
+        if ((playerScore === 5) || (computerScore === 5)) {
             //document.querySelector('#game').textContent = "GAME OVER";
 
         // print final scores
         if (playerScore === computerScore) {
-            document.querySelector('#game').textContent = "GAME OVER | It's a draw!";
+            document.querySelector('#game').textContent = "GAME OVER\r\nIt's a draw";
             }
             else if (playerScore > computerScore) {
-                document.querySelector('#game').textContent = "GAME OVER | Player Wins!";
+                document.querySelector('#game').textContent = "GAME OVER\r\nPlayer Wins";
             
             }
             else if (computerScore > playerScore) {
-                document.querySelector('#game').textContent = "GAME OVER | Computer Wins!";
+                document.querySelector('#game').textContent = "GAME OVER\r\nComputer Wins";
             }
 
             // Mark that the game is over and don't let the RPS buttons work
